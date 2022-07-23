751556fbbdf684b3a3aee6ec25169f3dbc382b74
MaskeZen <91011274+MaskeZen945961@users.noreply.github.com>
2022-07-23 20:23:16 +0200

    /\

--- discord nitro gen.py
+++ discord nitro gen.py
@@ -1,19 +1,24 @@
 import random
 import string
+from discord_webhook import DiscordWebhook
+import time
 
-fic = open("number.txt", "w")
-
-while True:
-    code = "".join(random.choices(
-    string.ascii_uppercase + string.digits + string.ascii_lowercase,
-    k = 16
-    ))
-    fic.write(f"https://discord.gift/{code}\n")
-    print(f"https://discord.gift/{code}\n")
+def wait(seconds):
+    time.sleep(seconds)
 
 
+def main():
+    webhock = input ("Webhook: ") 
+
+    while True:
+        code = "".join(random.choices(
+        string.ascii_uppercase + string.digits + string.ascii_lowercase,
+        k = 16
+        ))
+        print(f"https://discord.gift/{code}\n")
+        webhook = DiscordWebhook(webhock, content=f"https://discord.gift/{code}")
+        response = webhook.execute()
+        wait(1) #wait 1 seconde before sending the next message delete this if you want to send the message instantly (bug)
 
 
-
-
-
+main()
